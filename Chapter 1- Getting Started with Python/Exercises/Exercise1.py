a04224e882672f9c333cd3b925ae197609114e8f
Zahra Muhammad <111445463+ohh-zara@users.noreply.github.com>
2023-09-28 11:58:21 +0400

    Exercise1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -0,0 +1,21 @@
+# Chapter 1 Exercises Answer:
+
+#Exercises with a tick mark :ballot_box_with_check: represent exercises that must be submitted as part of your Programming Skills Portfolio as a minimum expectation. Completing more exercises provides the opportunity to attain higher marks. For each exercise you should create a _**new project**_ with the name of the exercise and save it to this exercises folder in your local repository.
+#Once you have completed your solution you should make sure you commit and push your solutions to your remote repository on GitHub. You can commit and push as many changes to your solutions as you wish, only those pushed before the chapter deadlines will be marked for the Programming Skills Portfolio.  
+#Exercise 1: Print Strings :ballot_box_with_check:
+
+#Write a Python program to print the following string in a specific format.
+
+#Twinkle, twinkle, little star,
+#	How I wonder what you are! 
+#		Up above the world so high,   		
+#		Like a diamond in the sky. 
+#Twinkle, twinkle, little star, 
+#	How I wonder what you are
+
+print("""Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are""")
